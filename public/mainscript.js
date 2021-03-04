db6462395ff54c5c99ffa08d20759bbfbed1b06e
Otis Mario <otismario@flexwork.com.ng>
2021-03-04 21:02:38 +0400

    Music Button Added

---
+++ public/mainscript.js
@@ -0,0 +1,18 @@
+// This code manages the play and pause of the songs on the thumbnails of the home page
+var musicBox = document.getElementById('musicBox');
+var isPlaying = false;
+
+// Ths is function will toggle between whenthe music is played and when it is paused
+function togglePlay() {
+    isPlaying ? musicBox.onpause() : musicBox.play();
+};
+
+musicBox.onplaying = function() {
+    isPlaying = true;
+    document.getElementById('musicBtn').innerHTML="Pause";
+};
+musicBox.onpause = function() {
+    isPlaying = false;
+    document.getElementById('musicBtn').innerHTML="Play";
+    
+};
